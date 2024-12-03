ace8d9ee4f0493b47697bfeb44c42e812e002fb3
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-03 16:04:15 +0500

    .

--- App.js
+++ App.js
@@ -1,3 +1,35 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import HomeScreen from './screens/HomeScreen';
+// import QuizScreen from './screens/QuizScreen';
+// import ResultScreen from './screens/ResultScreen';
+
+// export type RootStackParamList = {
+//   Home: undefined;
+//   Quiz: { category: string; difficulty: string };
+//   Result: { score: number; totalQuestions: number };
+// };
+
+// const Stack = createStackNavigator<RootStackParamList>();
+
+// const App: React.FC = () => {
+//   return (
+//     <NavigationContainer>
+//     <Stack.Navigator initialRouteName= "Home" >
+//     <Stack.Screen name="Home" component = { HomeScreen } options = {{ title: 'Quiz App' }
+// } />
+//   < Stack.Screen name = "Quiz" component = { QuizScreen } options = {{ title: 'Quiz' }} />
+//     < Stack.Screen name = "Result" component = { ResultScreen } options = {{ title: 'Quiz Result' }} />
+//       </Stack.Navigator>
+//       </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -17,13 +49,24 @@ const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Quiz App' }} />
-        <Stack.Screen name="Quiz" component={QuizScreen} options={{ title: 'Quiz' }} />
-        <Stack.Screen name="Result" component={ResultScreen} options={{ title: 'Quiz Result' }} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Quiz App' }}
+        />
+        <Stack.Screen
+          name="Quiz"
+          component={QuizScreen}
+          options={{ title: 'Quiz' }}
+        />
+        <Stack.Screen
+          name="Result"
+          component={ResultScreen}
+          options={{ title: 'Quiz Result' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
